ddb111165dcd70b64ef5337e17159c30fe3c815a
Rohit Manohar <manoharrohit372@gmail.com>
2024-11-05 01:04:06 +0530

    donr

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -98,7 +98,7 @@ export default function Education() {
         {
           opacity: 1,
           y: 0,
-          duration: 1,
+          duration: 1.8,
           stagger: 0.2,
           ease: "power2.out",
           scrollTrigger: {
